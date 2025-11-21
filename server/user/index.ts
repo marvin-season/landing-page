de787f7f82e62def4406726c865be3a58b4891da
Marvin <2764876579@qq.com>
2025-11-21 15:24:14 +0800

    feat: 剔除prisma, 归档在分支 feature/archive

--- server/user/index.ts
+++ server/user/index.ts
@@ -1,7 +1,15 @@
 import { publicProcedure, router } from "~/server/trpc";
 
 export const userRouter = router({
-  list: publicProcedure.query(async ({ ctx: {db} }) => {
-    return await db.user.findMany();
-  })
+  list: publicProcedure.query(async () => {
+    return [{
+      id: "1",
+      name: "John Doe",
+      email: "john.doe@example.com",
+    }, {
+      id: "2",
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+    }];
+  }),
 });
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,23 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/utils/trpc";
 import { MotionDiv } from "../../components/motion";
-import Content from "./_components/content";
 
-console.log("MotionDiv", MotionDiv);
-export default async function AdminPage() {
+export default function AdminPage() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.user.list.queryOptions());
   return (
     <MotionDiv
       initial={{ opacity: 0, y: 48 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -48 }}
     >
-      <Content />
+      {data?.map((user) => (
+        <div key={user.id}>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </MotionDiv>
   );
 }
