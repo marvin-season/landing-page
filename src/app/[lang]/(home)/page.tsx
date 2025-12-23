6ecfad583ff7a5f9ebf8c3cc3993d74a9d7c68be
Marvin <2764876579@qq.com>
2025-12-23 19:57:39 +0800

    feat: clear home page

--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -1,10 +1,9 @@
-import { Mine } from "@/app/[lang]/(home)/_components/mine";
 import type { PageLangParam } from "@/lib/i18n/appRouterI18n";
 import { getI18nInstance } from "@/lib/i18n/appRouterI18n";
 
 export default async function Home({ params }: PageLangParam) {
   const lang = (await params).lang;
+  // biome-ignore lint/correctness/noUnusedVariables: <i18n>
   const i18n = getI18nInstance(lang);
-  console.log(i18n);
-  return <Mine />;
+  return <>Home</>;
 }
