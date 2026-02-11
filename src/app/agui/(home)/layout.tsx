b7eff90c050ed818746d6441c5e3d6a292bbc57d
Marvin <2764876579@qq.com>
2026-02-11 14:24:38 +0800

    fix: 移除旧的路由

--- src/app/agui/(home)/layout.tsx
+++ src/app/agui/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import { AgentProvider } from "@/app/agui/_components/AgentProvider";
 import "@/css/globals.css";
-import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -10,15 +9,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="h-dvh overflow-scroll">
-        <nav className="flex gap-4 p-4 border-b">
-          <Link href="/agui" className="hover:underline">
-            Home
-          </Link>
-
-          <Link href="/agui/raw" className="hover:underline">
-            Raw AG-UI
-          </Link>
-        </nav>
         <AgentProvider>{children}</AgentProvider>
       </body>
     </html>
